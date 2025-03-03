4f441172bfabb28b6cf0c28cb314f2ce0875dfcd
kelviy <52874397+kelviy@users.noreply.github.com>
2025-03-03 15:41:14 +0200

    Converted to primative arrays from ArrayList

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
@@ -18,11 +17,15 @@ public class Main {
 
     public static void repl_loop(BufferedReader kb ,Matrix matrix) {
         String ans = "";
-        String menu = "-----MENU-----\n" +
-                "1. Calculate Determinate\n" +
-                "2. Gaussian Elimination\n" +
-                "3. Renter Matrix\n" +
-                "q. Quit";
+        String menu = """
+                -----MENU-----
+                1. Print Matrix again
+                2. Calculate Determinate
+                3. Transform to Gaussian Elimination
+                4. Transpose Matrix
+                5. Multiply with another Matrix
+                6. Renter Matrix
+                q. Quit""";
 
         while (!ans.equals("q")) {
             System.out.println(menu);
@@ -33,14 +36,28 @@ public class Main {
 
                 switch (ans) {
                     case "1":
-                        System.out.println("The determinate of the matrix is: " + matrix.getDeterminate());
-                        break;
-                    case "2":
-                        System.out.println("Row Echelon Matrix");
-                        matrix.guassianMatrix();
                         System.out.println(matrix.printMatrix());
                         break;
+                    case "2":
+                        System.out.println("The determinate of the matrix is: " + matrix.getDeterminate());
+                        break;
                     case "3":
+                        System.out.println("Row Echelon Matrix");
+                        matrix.gaussianMatrix();
+                        System.out.println(matrix.printMatrix());
+                        break;
+                    case "4":
+                        matrix = matrix.transpose();
+                        System.out.println(matrix.printMatrix());
+                        break;
+                    case "5":
+                        System.out.println("Enter new Matrix:");
+                        Matrix newMatrix = new Matrix(inputMatrix(kb));
+                        matrix = matrix.multiply(newMatrix);
+                        System.out.println("Result of multiplication:");
+                        System.out.println(matrix.printMatrix());
+                        break;
+                    case "6":
                         matrix = new Matrix(inputMatrix(kb));
                         System.out.println("Matrix inputted: ");
                         System.out.println(matrix.printMatrix());
@@ -51,10 +68,10 @@ public class Main {
         }
     }
 
-    public static ArrayList<ArrayList<Double>>  inputMatrix(BufferedReader kb) {
-        ArrayList<ArrayList<Double>> matrix = new ArrayList<>();
+    public static double[][]  inputMatrix(BufferedReader kb) {
         String ans = "";
         String errorMsg = "Invalid answer. Try again.";
+        double[][] matrix = null;
 
         try {
             System.out.println("Enter your matrix dimensions...");
@@ -85,6 +102,8 @@ public class Main {
                 }
             }
 
+            matrix = new double[rows][cols];
+
             // Enter matrix
             System.out.println("Now enter matrix data. Enter each row of the matrix with each element separated by a space. (press b to re-enter previous row)");
             System.out.println("Enter rows (top down): ");
@@ -94,13 +113,13 @@ public class Main {
                 ans = kb.readLine();
 
                 String[] elements = ans.split(" ");
-                ArrayList<Double> row = new ArrayList<>();
+                double[] row = new double[cols];
 
                 for (int j = 0; j < cols; j++) {
-                    row.add(Double.parseDouble(elements[j]));
+                    row[j] = Double.parseDouble(elements[j]);
                 }
 
-                matrix.add(row);
+                matrix[i] = row;
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -116,5 +135,6 @@ public class Main {
             return false;
         }
     }
+
 
 }
--- src/Matrix.java
+++ src/Matrix.java
@@ -1,12 +1,16 @@
 import java.util.ArrayList;
 
 public class Matrix {
-    private ArrayList<ArrayList<Double>> matrix = new ArrayList<>();
+    private final double[][] matrix;
+    private int rows;
+    private int columns;
 
-    public Matrix(ArrayList<ArrayList<Double>> matrix) {
-        int columns = matrix.get(0).size();
-        for (int i = 1; i < matrix.size(); i++) {
-            if (columns != matrix.get(i).size()) {
+    public Matrix(double[][] matrix) {
+        this.columns = matrix[0].length;
+        this.rows = matrix.length;
+
+        for (int i = 1; i < matrix.length; i++) {
+            if (this.columns != matrix[i].length) {
                 throw new IllegalArgumentException("Matrix Rows are not consistent with the Matrix Columns. i.e: Matrix rows are not the same size");
             }
         }
@@ -17,9 +21,9 @@ public class Matrix {
         StringBuilder print = new StringBuilder();
         int padding = 0;
 
-        for (ArrayList<Double> row : matrix) {
-            for (Double element: row) {
-                String digits = String.valueOf(element);
+        for (int i = 0; i < this.rows; i++) {
+            for (int j = 0; j < this.columns; j++) {
+                String digits = String.valueOf(this.matrix[i][j]);
                 if (padding < digits.length()) {
                     padding = digits.length();
                 }
@@ -28,63 +32,57 @@ public class Matrix {
 
         padding += 3;
 
-        for (ArrayList<Double> row : matrix) {
-            for (Double element: row) {
-                print.append(String.format("%-"+padding+"s",element));
+        for (int i = 0; i < this.rows; i++) {
+            for (int j = 0; j < this.columns; j++) {
+                print.append(String.format("%-"+padding+"s",matrix[i][j]));
             }
             print.append("\n");
         }
         return print.toString();
     }
 
-    public double getElement(int i, int j) {
-        return matrix.get(i).get(j);
-    }
-
-    public int getRowNum() {
-        return matrix.size();
-    }
-
-    public int getColumnNum() {
-        return matrix.get(0).size();
-    }
-
-
     // recursive algorithm definition
     // go through first row
     // first element * | sgn | * | smaller mtrx | + second ...
     // base if 2x2 matrix. det = [0][0]*[1][1] + [0][1]*[1][0]
     public double getDeterminate() {
-        if (matrix.size() != matrix.get(0).size()) {
+        if (this.rows != this.columns) {
             throw new IllegalArgumentException("Matrix is not a square matrix");
         }
 
         // base case
-        if (matrix.size() == 2 && matrix.get(0).size() == 2) {
-            return getElement(0, 0) * getElement(1, 1) - getElement(0, 1) * getElement(1, 0);
+        if (this.rows == 2) {
+            return this.matrix[0][0] * this.matrix[1][1] - this.matrix[0][1] * this.matrix[1][0];
         }
 
         double determinate = 0;
 
-        for (int i = 0; i < matrix.size(); i++) {
-            determinate += getElement(0, i) * Math.pow(-1, 1 + i + 1) * minorMatrix(0, i).getDeterminate();
+        for (int i = 0; i < this.columns; i++) {
+            determinate += this.matrix[0][i] * Math.pow(-1, 1 + i + 1) * minorMatrix(0, i).getDeterminate();
         }
 
         return determinate;
     }
 
     public Matrix minorMatrix(int row, int column) {
-        ArrayList<ArrayList<Double>> newMatrix = new ArrayList<>();
+        int newRows = this.rows-1;
+        int newColumns = this.columns-1;
+        double[][] newMatrix = new double[newRows][newColumns];
 
-        for (int i = 0; i < getRowNum(); i++) {
+        int index_i = 0;
+        int index_j = 0;
+        for (int i = 0; i < this.rows; i++) {
             if (i != row) {
-                ArrayList<Double> newRow = new ArrayList<>();
-                for (int j = 0; j < getColumnNum(); j++) {
+                double[] newRow = new double[newColumns];
+                for (int j = 0; j < this.columns; j++) {
                     if (j != column) {
-                        newRow.add(getElement(i, j));
+                        newRow[index_j] = this.matrix[i][j];
+                        index_j++;
                     }
                 }
-                newMatrix.add(newRow);
+                index_j = 0;
+                newMatrix[index_i] = newRow;
+                index_i++;
             }
         }
         return new Matrix(newMatrix);
@@ -95,55 +93,114 @@ public class Matrix {
     // make column element to be 0 except the first diagonal. Using valid row reduction operations (1. swap, 2. add row to another row, 3, multiply row by scalar)
     // continue along the diagonal until the end
     // known problem. If too big sometimes the numbers will go to infinity or 0 and mess up the calculation. Maybe some scalling rules will solve the problem
-    public void guassianMatrix() {
-        int pivots = Math.min(getRowNum(), getColumnNum());
+    public void gaussianMatrix() {
+        int pivots = Math.min(this.rows, this.columns);
 
         for (int i = 0; i < pivots; i++) {
-            double pivotElement = getElement(i, i);
-            for (int j = i+1; j < getRowNum(); j++) {
-                double scalar = getScalarReduction(pivotElement, getElement(j, i));
+//            int largestRow = i;
+//            for (int j = i+1; j < getRowNum(); j++) {
+//                if (Math.abs(getElement(j, i)) < Math.abs(getElement(largestRow, i))) {
+//                    largestRow = j;
+//                }
+//            }
+//            swapRows(largestRow, i);
+
+            double pivotElement = this.matrix[i][i];
+
+            // moves 0 pivot row to bottom and swaps to non-zero pivot. if non-available then skip pivot
+            if (pivotElement==0) {
+                for(int j = this.rows-1; j > i; j--) {
+                    if (this.matrix[j][i] != 0) {
+                        swapRows(i, j);
+                        break;
+                    }
+                }
+                pivotElement = this.matrix[i][i];
+                if (pivotElement==0) {
+                    continue;
+                }
+            }
+
+            for (int j = i+1; j < this.rows; j++) {
+                double scalar = getScalarReduction(pivotElement, this.matrix[j][i]);
                 addRowToAnother(i, j, scalar);
             }
         }
     }
 
     public Matrix deepCopy() {
-        ArrayList<ArrayList<Double>> newMatrix = new ArrayList<>();
-        for (int i = 0; i < getRowNum(); i++) {
-            ArrayList<Double> newRow = new ArrayList<>();
-            for (int j = 0; j < getColumnNum(); j++) {
-                newRow.add(getElement(i, j));
+        double[][] newMatrix = new double[this.rows][this.columns];
+        for (int i = 0; i < this.rows; i++) {
+            double[] newRow = new double[this.columns];
+            for (int j = 0; j < this.columns; j++) {
+                newRow[j] = this.matrix[i][j];
             }
-            newMatrix.add(newRow);
+            newMatrix[i] = newRow;
         }
         return new Matrix(newMatrix);
     }
 
     public void swapRows(int row1, int row2) {
-        ArrayList<Double> temp = matrix.get(row1);
-        matrix.set(row1, matrix.get(row2));
-        matrix.set(row2, temp);
+        if (row1 == row2) {
+            return;
+        }
+        double[] temp = this.matrix[row1];
+        this.matrix[row1] = this.matrix[row2];
+        this.matrix[row2] = temp;
     }
 
     //row2 + row1 * scalar
     public void addRowToAnother(int row1, int row2, double scalar) {
-        for (int i = 0; i < getColumnNum(); i++) {
-            double element1 = getElement(row1, i);
-            double element2 = getElement(row2, i);
+        for (int i = 0; i < this.columns; i++) {
+            double element1 = this.matrix[row1][i];
+            double element2 = this.matrix[row2][i];
             double newElement = element2 + scalar * element1;
-            matrix.get(row2).set(i, newElement);
+            this.matrix[row2][i] = newElement;
         }
     }
 
     public void multiplyRow(int row, double scalar) {
-        for (int i = 0; i < getColumnNum(); i++) {
-            double newElement = getElement(row, i)*scalar;
-            matrix.get(row).set(i, newElement);
+        for (int i = 0; i < this.columns; i++) {
+            double newElement = this.matrix[row][i]*scalar;
+            this.matrix[row][i] = newElement;
         }
     }
 
     // determine scalar such that row below is zero
     private double getScalarReduction(double pivot, double num) {
         return -num/pivot;
+    }
+
+    public Matrix transpose() {
+        double[][] newMatrix = new double[this.columns][this.rows];
+
+        for (int i = 0; i < this.columns; i++) {
+            double[] newRow = new double[this.rows];
+            for (int j = 0; j < this.rows; j++) {
+                newRow[j] = matrix[j][i];
+            }
+            newMatrix[i] = newRow;
+        }
+        return new Matrix(newMatrix);
+    }
+
+    public Matrix multiply(Matrix other) {
+        if (this.columns != other.rows) {
+            throw new IllegalArgumentException("Invalid Matrix dimensions for multiplication");
+        }
+        double[][] newMatrix = new double[this.rows][other.columns];
+
+        for (int i = 0; i < newMatrix.length; i++) {
+            double[] newRow = new double[other.columns];
+            for (int j = 0; j < newMatrix[0].length; j++) {
+                newRow[j] = 0;
+                for (int k = 0; k < this.columns; k++) {
+                    newRow[j] += this.matrix[i][k]*other.matrix[k][j];
+                }
+            }
+            newMatrix[i] = newRow;
+        }
+
+        return new Matrix(newMatrix);
     }
 }
